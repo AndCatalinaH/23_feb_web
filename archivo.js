7e16b11f94c08071cf6b125196c03529fc555408
AndCatalinaH <andhernandezr@unal.edu.co>
2026-02-23 12:49:58 -0500

    Funciones en el archivo .js

--- archivo.js
+++ archivo.js
@@ -1,3 +1,16 @@
-function prueba(){
-    alert("Hola desde JS");
+function suma(){
+    /*alert("Hola desde JS");*/
+
+    /*Concatena los dos valores, osea los junta */
+    var a1 = document.getElementById("a1").value;
+    var a2 = document.getElementById("a2").value;
+    alert(a1+a2);
+
+    /*Cambio de variable */
+    a1 = parseInt(a1);
+    a2 = parseInt(a2);
+    alert(a1+a2);
+
+
+    
 }
